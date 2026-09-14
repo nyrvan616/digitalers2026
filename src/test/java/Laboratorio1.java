d2452c8db6871b71ecd5c88444f266be8fb7f598
Nicolas Fernandez Razzetto <fernandezr.nicolas@gmail.com>
2026-09-14 20:11:38 -0300

    Primera version

---
+++ src/test/java/Laboratorio1.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
+
+
+
+public class Laboratorio1 {
+	/*Esto es un comentario*/
+	@Test
+	void lab1_test() {
+		System.out.println("Hola Mundo!!!");
+	}
+	
+	@Test
+	void lab1_E1() {
+		WebDriver driver = new ChromeDriver();
+	
+			try {
+			driver.get("https://automationexercise.com/");
+			driver.manage().window().maximize();
+			
+		} finally {
+			driver.quit();
+		} 	
+	} 
+	
+	@Test
+	void lab1_E2() {
+		WebDriver driver = new FirefoxDriver();
+	
+			try {
+			driver.get("https://automationexercise.com/");
+			driver.manage().window().maximize();
+			
+		} finally {
+			driver.quit();
+		} 	
+	}
+	
+	@Test
+	void lab1_E2_plus() {
+		WebDriver driver = new EdgeDriver();
+	
+			try {
+			driver.get("https://automationexercise.com/");
+			driver.manage().window().setSize(new Dimension(1500, 1000));
+		} finally {
+			driver.quit();
+		} 	
+	} 
+	
+	@Test
+	void lab1_E3() {
+		WebDriver driver = new ChromeDriver();
+		
+		try {
+			driver.get("https://automationexercise.com/products");
+			
+			WebElement buscador = driver.findElement(By.id("search_product"));
+			buscador.sendKeys("Blue Top");
+			
+			WebElement botonBuscar = driver.findElement(By.id("submit_search"));
+			botonBuscar.click();
+			
+		} finally {
+			driver.quit();			
+		}
+	}
+	
+	@Test
+	void lab1_E4() {
+	WebDriver driver = new ChromeDriver();
+	try {
+	driver.get("https://automationexercise.com/products");
+	
+	WebElement buscador = driver.findElement(By.id("search_product"));
+	buscador.sendKeys("Blue Top");
+	buscador.sendKeys(Keys.ENTER);
+	
+
+	} finally {
+	driver.quit();
+	}
+	}
+}
